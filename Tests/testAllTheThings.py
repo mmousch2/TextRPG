fc5b38229a08ef8a0056b135767486ac29af331c
nfortma2 <kocheva2@illinois.edu>
2016-04-22 00:59:15 -0500

    mo tests

--- Tests/testAllTheThings.py
+++ Tests/testAllTheThings.py
@@ -26,3 +26,20 @@ class TestControllerCommands(unittest.TestCase):
     def test_npc_setup(self):
         self.assertTrue("Vivi" in self.convotree, "Vivi is not in convotree")
         self.assertTrue(self.npc.name == "Vivi")
+
+    def room_test(self):
+        self.room.set_name("my room")
+        assert(self.room.name == "my room")
+
+        self.room.set_description("a beautiful room")
+        assert(self.room.description == "a beautiful room")
+
+        self.room.add_direction("west", 42)
+        assert("west" in self.room.directions)
+        assert(self.room.directions["west"] == 42)
+
+        self.room.add_item("shiny object")
+        assert("shiny object" in self.room.items)
+
+        self.room.add_character("steve")
+        assert("steve" in self.room.characters)
